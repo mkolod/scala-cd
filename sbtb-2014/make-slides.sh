0c12b29353cf58ee1210cba4c5413571acdc8f5b
James Earl Douglas <james@earldouglas.com>
2014-08-08 07:18:05 -0700

    Add SBTB 2014 slides

---
+++ sbtb-2014/make-slides.sh
@@ -0,0 +1,11 @@
+OUTPUT_FILE=slides.html
+
+pandoc -s -f markdown -t slidy -o $OUTPUT_FILE README.md
+sed -i 's/<\/head>/  <style type="text\/css">p.caption { font-style: italic; }<\/style>\n<\/head>/g' $OUTPUT_FILE
+sed -i 's/<\/head>/  <style type="text\/css">div.figure { text-align: center; }<\/style>\n<\/head>/g' $OUTPUT_FILE
+
+wget -nc http://www.w3.org/Talks/Tools/Slidy2/styles/slidy.css
+wget -nc http://www.w3.org/Talks/Tools/Slidy2/scripts/slidy.js
+
+sed -i 's/http:\/\/www.w3.org\/Talks\/Tools\/Slidy2\/styles\///g' $OUTPUT_FILE
+sed -i 's/http:\/\/www.w3.org\/Talks\/Tools\/Slidy2\/scripts\/slidy.js(.gz)?/slidy.js/g' $OUTPUT_FILE
