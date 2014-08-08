978c427285ee00f825ef0717654be422f443acb6
James Earl Douglas <james@earldouglas.com>
2014-08-08 09:39:23 -0700

    Gray strikethrough

--- sbtb-2014/make-slides.sh
+++ sbtb-2014/make-slides.sh
@@ -3,6 +3,7 @@ OUTPUT_FILE=index.html
 pandoc -s -f markdown -t slidy -o $OUTPUT_FILE README.md
 sed -i 's/<\/head>/  <style type="text\/css">p.caption { font-style: italic; }<\/style>\n<\/head>/g' $OUTPUT_FILE
 sed -i 's/<\/head>/  <style type="text\/css">div.figure { text-align: center; }<\/style>\n<\/head>/g' $OUTPUT_FILE
+sed -i 's/<\/head>/  <style type="text\/css">strike { color: #999; }<\/style>\n<\/head>/g' $OUTPUT_FILE
 
 wget -nc http://www.w3.org/Talks/Tools/Slidy2/styles/slidy.css
 wget -nc http://www.w3.org/Talks/Tools/Slidy2/scripts/slidy.js
